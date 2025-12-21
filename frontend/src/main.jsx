6295245c199eecf78315edcd1c83eaf49836e5b6
shreyas-3007 <shreyaspatil3007@gmail.com>
2025-12-21 11:58:47 +0530

    Done basic configs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import {ToastContainer} from 'react-toastify'
+import {BrowserRouter} from 'react-router'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <div className='container'>
+        <App />
+        <ToastContainer autoClose={2000}/>
+    </div>
+  </BrowserRouter>,
 )
