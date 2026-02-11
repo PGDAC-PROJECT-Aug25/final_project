1b322acc47465663dc311505f70e4b3e153c072b
Sahil Pujari <pujarisahil21@gmail.com>
2026-02-11 09:50:22 +0530

    updated functionality

--- backend/src/main/java/com/backend/dto/AdminUserResponse.java
+++ backend/src/main/java/com/backend/dto/AdminUserResponse.java
@@ -2,29 +2,21 @@ package com.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AdminUserResponse {
-<<<<<<< HEAD
-
-    private Long userId;
-=======
     private Long  userId;
->>>>>>> 28518f940a4d2252052a68634023d2daf07a7ddf
     private String name;
     private String  email;
     private String  role;
     private Boolean isActive;
-<<<<<<< HEAD
-    private Boolean emailVerified;
+    private Boolean  emailVerified;
 
-    // --- New fields for Provider ---
     private Long providerId;
     private Boolean isProviderVerified;
-=======
-    private Boolean  emailVerified;
->>>>>>> 28518f940a4d2252052a68634023d2daf07a7ddf
 }
