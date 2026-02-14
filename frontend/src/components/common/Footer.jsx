6d4d03f585e1ae739ae7b3e39435474c0132280a
Sahil Pujari <pujarisahil21@gmail.com>
2026-02-14 12:55:57 +0530

    changes in footer

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
       </div>
       
       <div className="footer-bottom">
-        <p>&copy; 2024 Bus Booking System. All rights reserved.</p>
+        <p>&copy; 2026 Bus Booking System. All rights reserved.</p>
       </div>
     </footer>
   );
